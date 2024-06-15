9a4849ad9208a0a9619eb45f4c2c054132cc2fee
Radouane Abounouas <radouaneabn@gmail.com>
2024-06-15 22:29:41 +0000

    Task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -7,8 +7,6 @@ BEGIN
 	DECLARE n_projects INT;
 
 	SELECT SUM(score), COUNT(*) INTO total_score, n_projects FROM corrections c where c.user_id = user_id;
-	SELECT n_projects AS 'Number of Projects';
-	SELECT total_score AS 'TOTALE';
 	if n_projects > 0 THEN
 		UPDATE users SET average_score = total_score / n_projects WHERE id = user_id;
 	end if;
